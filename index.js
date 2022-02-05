6f4f1bc2d0bf6828bac62b8fb4f42d299db8d2ee
NoahSolis <NSolis1993@gmail.com>
2022-02-04 20:01:34 -0600

    commit tree 872c61e3661803da473110dd139d980859a08935

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat() {
+    const newCats = [...cats, "Broom"];
+    return newCats;
+}
+
+function prependCat() {
+    const newNewCats = ["Arnold", ...cats];
+    return newNewCats;
+}
+
+function removeLastCat() {
+    const bye = cats.slice(0, cats.length - 1);
+    return bye;
+}
+
+function removeFirstCat() {
+    const byeOne = cats.slice(1);
+    return byeOne;
+}
